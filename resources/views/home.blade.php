9b8e16594b3b39ef9f16e258d45f652b17e4d121
FATIMA ZAHRA <1728829@alu.murciaeduca.es>
2025-11-11 09:15:31 +0100

    vista home integradaen Dopetrope

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,19 +2,10 @@
 
 @section('menu')
     @parent
-    <p>¡Hola {{ $nombre ?? 'colega' }}!</p>
+    <li>Opcion adicional</li>
 @endsection
 
 @section('content')
-    <ul>
-        @if (count($users) === 1)
-            <li>Solo hay un usuario! </li>
-            <li>Usuario {{ $users['name'] }} con identificador: {{ $users['id'] }}</li>
-        @elseif (count($users) > 1)
-            Hay muchos usuarios!
-            @include('users.usersList', ['users' => $users])
-        @else
-            <li> No hay ningún usuario :(</li>
-        @endif
-    </ul>
+    <h2>Marca personal FP</h2>
+    <p>Pagina principal</p>
 @endsection
