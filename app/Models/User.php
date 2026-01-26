41f899252b0a108149718c504a772fdc5b7f7dcd
FATIMA ZAHRA <1728829@alu.murciaeduca.es>
2026-01-26 08:30:44 +0100

    Creacion de relacion 1 a 1 User-Curriculo

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,7 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function curriculo(){
+        return $this->hasOne(Curriculo::class);
+    }
 }
